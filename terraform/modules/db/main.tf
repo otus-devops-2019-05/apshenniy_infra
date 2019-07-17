381b9c2f263f3c1c962f0d153ab99c0cf3eb4f2a
Aleksey Pshenniy <apshenniy@gmail.com>
2019-07-17 23:03:45 +0300

    fix

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,8 +1,7 @@
 resource "google_compute_instance" "db" {
   name         = "reddit-db"
   machine_type = "g1-small"
-
-  #zone = "${var.zone}"
+  zone = "${var.zone}"
   tags = ["reddit-db"]
 
   boot_disk {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,25 +1,10 @@
-variable project {
-  description = "Project ID"
-}
-
-variable region {
-  description = "Region"
-  default     = "europe-west1"
-}
-
-variable public_key_path {
-  description = "Path to the public key used for ssh access"
-}
-
-variable private_key_path {
-  description = "Path to the privat key used for ssh access"
-}
-
-variable disk_image {
-  description = "Disk image"
-}
-
-variable zone {
-  description = "Resource Terraform"
-  default     = "europe-west1-b"
-}
+variable "project" {
+    type        = "string"
+    description = "Project ID"
+  }
+  
+  variable "region" {
+    type        = "string"
+    description = "region"
+    default     = "europe-west1"
+  }
